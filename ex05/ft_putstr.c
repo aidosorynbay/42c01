2d9b3b9df7bf4a331320f2a012dc494ff03784bf
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 18:19:04 +0400

    ex05_FINAL

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 17:54:21 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/02 17:54:27 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+// #include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+// int	main(void)
+// {
+// 	char str1[] = "Hello, world!";
+// 	ft_putstr(str1);
+// }
