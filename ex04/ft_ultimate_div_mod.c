3b44533c3e75454b0c4fcafe99a5ca3432b7fc3e
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 17:09:37 +0400

    ex04_main

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 16:56:31 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/02 16:56:32 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 42;
+	b = 4;
+	printf("%i\n", a);
+	printf("%i\n\n", b);
+	ft_ultimate_div_mod(&a, &b);
+	printf("%i\n", a);
+	printf("%i", b);
+}
