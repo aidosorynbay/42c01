67528b97423518ec2264f6f441e1469cdf7c834d
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 16:34:52 +0400

    ex03_finalfinal

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+// #include <unistd.h>
+// #include <stdio.h>
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
