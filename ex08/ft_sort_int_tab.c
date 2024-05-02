38a3db0f48b26b088518dd15531678a97da4795a
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 22:30:40 +0400

    ex08_FINAL

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 20:52:17 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/02 20:52:22 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+void	swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			swap(&tab[i], &tab[i + 1]);
+			i = 0;
+		}
+		i++;
+	}
+}
+
+// int	main(void)
+// {
+// 	int n_array[] = {0, 3, 5, 1, 2, 4};
+// 	ft_sort_int_tab(n_array, 6);
+// }
