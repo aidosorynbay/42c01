7fb7f0e94cb1cee819a7cbf4c5bb8f6ff5a286b3
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 16:31:37 +0400

    ex03_main

---
+++ ex03/ft_div_mod.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 16:29:32 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/02 16:29:36 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 42;
+	b = 4;
+	ft_div_mod(a, b, &div, &mod);
+	printf("%i\n", div);
+	printf("%i", mod);
+}
