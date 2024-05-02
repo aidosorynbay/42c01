7ce781dc58be8aec252a26318bf4b217164f75da
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 17:22:06 +0400

    ex01_FINAL

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -10,7 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <unistd.h>
+// #include <stdio.h>
+
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
+
+// int	main(void)
+// {
+// 	int	a;
+// 	int	*a1 = &a;
+// 	int	**a2 = &a1;
+// 	int	***a3 = &a2;
+// 	int	****a4 = &a3;
+// 	int	*****a5 = &a4;
+// 	int	******a6 = &a5;
+// 	int	*******a7 = &a6;
+// 	int	********a8 = &a7;
+// 	int	*********a9 = &a8;
+// 	ft_ultimate_ft(a9);
+// 	printf("%i", a);
+// }
