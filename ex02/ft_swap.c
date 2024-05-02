6d68fd44057948ca3bac8a8018822237ad9f4380
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 16:01:26 +0400

    ex02_final

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -10,24 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-
 void	ft_swap(int *a, int *b)
 {
-	int temp;
+	int	temp;
+
 	temp = *b;
 	*b = *a;
 	*a = temp;
-}
-
-int main(void)
-{
-	int a = 4;
-	int b = 44;
-	printf("%i\n", a);
-	printf("%i\n\n", b);
-	ft_swap(&a, &b);
-	printf("%i\n", a);
-	printf("%i", b);
 }
