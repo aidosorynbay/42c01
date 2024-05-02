95e53ae9053de023e4946611ea9421cadb92bcb5
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-02 18:49:04 +0400

    ex06_FINAL

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 18:28:10 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/02 18:28:12 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+// #include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+// int	main(void)
+// {
+// 	char str1[] = "Hello, world!";
+// 	ft_strlen(str1);
+// }
